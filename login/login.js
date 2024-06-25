585eaefa5a9f9d9298e46092a53d11fed6467628
Jolo <148597145+Joloooo@users.noreply.github.com>
2024-06-25 19:01:01 +0200

    mrrobotfinalkinks

--- login/login.js
+++ login/login.js
@@ -3,40 +3,47 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('error-message');
 
-    // Simulate an HTTP request by using a fetch call to a non-existent endpoint
-    fetch('https://jsonplaceholder.typicode.com/posts', {
+    // Create a JSON object for the login data
+    const loginData = {
+        username: username,
+        password: password
+    };
+
+    // Replace with your deployed Firebase function URL
+    const functionURL = 'https://us-central1-mrrobot-1d1d4.cloudfunctions.net/validateLogin';
+
+    // Call the Firebase function
+    fetch(functionURL, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ username, password })
+        body: JSON.stringify(loginData)
     })
-    .then(response => {
-        // Simulate response
-        if (username === 'phillipprice' && password === '123456') {
-            return { success: true, user: 'phillipprice' };
-        } else if (username === 'tyrellwellick' && password === 'loveme1') {
-            return { success: true, user: 'tyrellwellick' };
-        } else {
-            return { success: false };
-        }
-    })
+    .then(response => response.json())
     .then(data => {
         if (data.success) {
-            if (data.user === 'phillipprice') {
+            if (username === 'phillipprice') {
                 window.location.href = 'phillipprice/phillipprice.html';
-            } else if (data.user === 'tyrellwellick') {
+            } else if (username === 'tyrellwellick') {
                 window.location.href = 'tyrellwellick/tyrellwellick.html';
             }
-        } else {
-            const errorMessage = document.getElementById('error-message');
+        } else {console.log(123)
+            errorMessage.textContent = data.message;
             errorMessage.style.display = 'block';
+            setTimeout(() => {
+                errorMessage.style.display = 'none';
+            }, 2000);
         }
     })
     .catch(error => {
         console.error('Error:', error);
-        const errorMessage = document.getElementById('error-message');
+        errorMessage.textContent = 'An error occurred';
         errorMessage.style.display = 'block';
+        setTimeout(() => {
+            errorMessage.style.display = 'none';
+        }, 2000);
     });
 });
