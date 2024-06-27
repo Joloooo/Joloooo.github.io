c8ad1f5673d7e9011eb9fbdc7ae5b55e0ebfd2ab
Jolo <148597145+Joloooo@users.noreply.github.com>
2024-06-27 03:03:17 +0200

    mrrobotfinalcommit

---
+++ mrrobot_challenge/mrrobot_challenge.js
@@ -0,0 +1,7 @@
+document.getElementById('beginner').addEventListener('click', function() {
+    window.location.href = 'beginner/beginner.html';
+});
+
+document.getElementById('advanced').addEventListener('click', function() {
+    window.location.href = 'advanced/advanced.html';
+});
