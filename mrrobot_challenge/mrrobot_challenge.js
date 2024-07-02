cbaec019d89edd68b744debc04be3c2d91cfa82d
Jolo <148597145+Joloooo@users.noreply.github.com>
2024-07-02 04:11:50 +0200

    reallyfinal_mr_robot

--- mrrobot_challenge/mrrobot_challenge.js
+++ mrrobot_challenge/mrrobot_challenge.js
@@ -2,6 +2,10 @@ document.getElementById('beginner').addEventListener('click', function() {
     window.location.href = 'beginner/beginner.html';
 });
 
+document.getElementById('adventurer').addEventListener('click', function() {
+    window.location.href = 'adventurer/adventurer.html';
+});
+
 document.getElementById('advanced').addEventListener('click', function() {
     window.location.href = 'advanced/advanced.html';
 });
