da1dfe556250eb91ede4849d6950f7920589f02d
joloooo <Zviadjolokhava@gmail.com>
2025-01-24 22:58:02 +0100

    Deploy static site

---
+++ mrrobot_challenge/mrrobot_challenge.js
@@ -0,0 +1,11 @@
+document.getElementById('beginner').addEventListener('click', function() {
+    window.location.href = 'beginner/beginner.html';
+});
+
+document.getElementById('adventurer').addEventListener('click', function() {
+    window.location.href = 'adventurer/adventurer.html';
+});
+
+document.getElementById('advanced').addEventListener('click', function() {
+    window.location.href = 'advanced/advanced.html';
+});
